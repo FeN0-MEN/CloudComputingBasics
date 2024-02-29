649379398e9da2722cf9a26c11ece6ced572f72b
Dmitry Starostenok <49001181+Hlebnica@users.noreply.github.com>
2024-02-29 15:45:28 +0500

    update comments

--- lab1/UDPSocketClient.py
+++ lab1/UDPSocketClient.py
@@ -8,29 +8,28 @@ def receive_employee_data(client_socket):
 
 def main():
     while True:
-        # Создаем UDP сокет
+        # UDP сокет
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        # Указываем адрес и порт сервера
+        # Адрес и порт сервера
         server_address = ('localhost', 12345)
 
         try:
-            # Отправляем запрос серверу
+            # Отправление запроса серверу
             message = "Запрос информации о работниках"
             print("Отправка запроса:", message)
             client_socket.sendto(message.encode(), server_address)
 
-            # Получаем информацию о работниках от сервера
+            # Получение информация о работниках от сервера
             employees = receive_employee_data(client_socket)
             print("Информация о работниках:")
             for employee in employees:
                 print(f"ID: {employee['id']}, Имя: {employee['name']}, Должность: {employee['position']}")
 
         finally:
-            # Закрываем соединение
             client_socket.close()
 
-        # Проверяем, хочет ли пользователь повторить запрос
+        # Повтор запроса
         choice = input("Хотите повторить запрос? (Да/Нет): ").lower()
         if choice != 'да':
             break
