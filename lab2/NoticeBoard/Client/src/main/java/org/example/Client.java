f7da93f5f1c060bbba12e672d2e47fb6d77596e6
Dmitry Starostenok <49001181+Hlebnica@users.noreply.github.com>
2024-03-07 12:12:46 +0500

    fix second task in lab2

--- lab2/NoticeBoard/Client/src/main/java/org/example/Client.java
+++ lab2/NoticeBoard/Client/src/main/java/org/example/Client.java
@@ -1,8 +1,22 @@
 package org.example;
+import java.io.*;
+import java.net.*;
 
 public class Client {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    try (Socket socket = new Socket("localhost", 8000);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+      String userInput;
+      while ((userInput = stdIn.readLine()) != null && !userInput.isEmpty()) {
+        out.println(userInput);
+        String serverResponse = in.readLine();
+        System.out.println("Server response: " + serverResponse);
+      }
+    } catch (IOException e) {
+      System.out.println("Ошибка при подключении к серверу: " + e.getMessage());
+    }
   }
 }
--- lab2/NoticeBoard/Server/src/main/java/org/example/Server.java
+++ lab2/NoticeBoard/Server/src/main/java/org/example/Server.java
@@ -6,40 +6,55 @@ import java.util.*;
 import com.google.gson.*;
 
 public class Server {
-  private static final String FILE_NAME = "messages.json";
+
+  private static final String FILE_NAME = "G:\\repos\\CloudComputingBasics\\lab2\\NoticeBoard\\Server\\src\\main\\java\\org\\example\\messages.json";
   private static List<String> messages = new ArrayList<>();
 
   public static void main(String[] args) {
     loadMessages();
 
     try (ServerSocket serverSocket = new ServerSocket(8000)) {
+      System.out.println("Сервер запущен...");
+
       while (true) {
-        try (Socket socket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-          String input = in.readLine();
-          if (input == null || input.isEmpty()) {
-            break;
-          } else if (input.equals("LIST")) {
-            out.println(String.join(";", messages));
-          } else {
-            messages.add(input);
-            saveMessages();
-            out.println("Message added: \"" + input + "\"");
-          }
-        } catch (IOException e) {
-          System.out.println("Ошибка при обработке запроса: " + e.getMessage());
-        }
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Подключен клиент: " + clientSocket);
+
+        handleClient(clientSocket);
       }
     } catch (IOException e) {
       System.out.println("Ошибка при запуске сервера: " + e.getMessage());
     }
   }
 
+  private static void handleClient(Socket clientSocket) {
+    try (BufferedReader in = new BufferedReader(
+        new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+      String input;
+      while ((input = in.readLine()) != null) {
+        if (input.equals("LIST")) {
+          out.println(String.join(";", messages));
+        } else if (!input.isEmpty()) {
+          messages.add(input);
+          saveMessages();
+          out.println("Message added: \"" + input + "\"");
+        } else {
+          break;
+        }
+      }
+      System.out.println("Клиент отключен: " + clientSocket);
+      clientSocket.close();
+    } catch (IOException e) {
+      System.out.println("Ошибка при обработке запроса: " + e.getMessage());
+    }
+  }
+
   private static void loadMessages() {
     try (Reader reader = new FileReader(FILE_NAME)) {
       Gson gson = new Gson();
-      messages = gson.fromJson(reader, new TypeToken<List<String>>(){}.getType());
+      messages = gson.fromJson(reader, new TypeToken<List<String>>() {
+      }.getType());
     } catch (IOException e) {
       System.out.println("Ошибка при загрузке сообщений: " + e.getMessage());
     }
