4b6073da1cf4681f77549e4c962392a1fc04a180
Dmitry Starostenok <49001181+Hlebnica@users.noreply.github.com>
2024-04-09 01:06:50 +0500

    lab3 update helloWorld

--- lab3_helloWorld/client.py
+++
@@ -1,4 +0,0 @@
-import xmlrpc.client
-
-with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
-    print(proxy.hello_world())
--- lab3_helloWorld/server.py
+++
@@ -1,9 +0,0 @@
-from xmlrpc.server import SimpleXMLRPCServer
-
-def hello_world():
-    return "Hello, World!"
-
-server = SimpleXMLRPCServer(("localhost", 8000))
-print("Запуск на порте 8000")
-server.register_function(hello_world, "hello_world")
-server.serve_forever()
